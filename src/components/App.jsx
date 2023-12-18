b6cb905e1f63e5ac2d967eadebf790f2d26cbca3
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-12-18 17:24:02 +0200

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { ContactForm } from './ContactForm';
+import { Filter } from './Filter';
+import { ContactList } from './ContactList';
+import s from './PhoneBook.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      setContacts(JSON.parse(savedContacts));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleChangeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const getFilteredContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const handleAddContact = newContact => {
+    if (contacts.map(c => c.name).includes(newContact.name)) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+    setContacts(prevState => [...prevState, newContact]);
+  };
+
+  const handleDeleteContact = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
+  };
+
+  const filteredContacts = getFilteredContacts();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={s.phonebook}>
+      <h1>Phonebook</h1>
+      <ContactForm handleAddContact={handleAddContact} />
+
+      <div className={s.contacts}>
+        <h2>Contacts</h2>
+        <Filter filter={filter} handleChangeInput={handleChangeFilter} />
+        <ContactList
+          filteredContacts={filteredContacts}
+          handleDeleteContact={handleDeleteContact}
+        />
+      </div>
     </div>
   );
 };
